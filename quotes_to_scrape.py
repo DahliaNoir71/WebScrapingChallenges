c2a7210010654ea391fc14810f24f7fb80b3fe94
DahliaNoir71 <serge.pfeiffer@proton.me>
2024-10-02 15:35:19 +0200

    refactoring

--- quotes_to_scrape.py
+++ quotes_to_scrape.py
@@ -1,9 +1,8 @@
 import requests
 from requests.auth import HTTPBasicAuth
-from bs4 import BeautifulSoup as bs
+from bs4 import BeautifulSoup
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
 import time
 
 BASE_URL = 'http://quotes.toscrape.com/'
@@ -14,13 +13,18 @@ SCROLL_PAUSE_TIME = 0.5
 URL_FIRST_QUOTE = 'https://quotes.toscrape.com/js/page/10/'
 
 
-def get_nb_pages():
-    #global base_url, username, password
+def fetch_page(url):
+    """Fetch page content with given URL using HTTP authentication."""
+    response = requests.get(url, auth=HTTPBasicAuth(USERNAME, PASSWORD))
+    return BeautifulSoup(response.text, 'html.parser')
+
+
+def count_pages():
+    """Count the number of paginated pages starting from BASE_URL."""
     nb_pages = 1
     current_url = BASE_URL
     while True:
-        response = requests.get(current_url, auth=HTTPBasicAuth(USERNAME, PASSWORD))
-        soup = bs(response.text, 'html.parser')
+        soup = fetch_page(current_url)
         li_next = soup.find('li', class_='next')
         if li_next:
             next_page_url = li_next.find('a')['href']
@@ -28,23 +32,18 @@ def get_nb_pages():
             nb_pages += 1
         else:
             break
+    print(f'Number of pages: {nb_pages}')
 
-    print('Nb pages : ' + str(nb_pages))
 
-def get_nb_quotations():
-    nb_quotes = 0
+def count_quotations():
+    """Scroll through the URL_SCROLL page and count the number of quotes."""
     browser = webdriver.Firefox()
     browser.get(URL_SCROLL)
-    # Get scroll height
     last_height = browser.execute_script("return document.body.scrollHeight")
+
     while True:
-        # Scroll down to bottom
         browser.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-
-        # Wait to load page
         time.sleep(SCROLL_PAUSE_TIME)
-
-        # Calculate new scroll height and compare with last scroll height
         new_height = browser.execute_script("return document.body.scrollHeight")
         if new_height == last_height:
             break
@@ -52,17 +51,16 @@ def get_nb_quotations():
 
     nb_quotes = len(browser.find_elements(By.CLASS_NAME, value='quote'))
     browser.close()
+    print(f'Number of quotations: {nb_quotes}')
 
-    print('Nb quotations : ' + str(nb_quotes))
 
-def get_first_quote():
-    response = requests.get(URL_FIRST_QUOTE)
-    soup = bs(response.text, 'html.parser')
+def print_first_quote():
+    """Fetch and print the first quote from URL_FIRST_QUOTE."""
+    soup = fetch_page(URL_FIRST_QUOTE)
     first_quote = soup.find('div', class_='quote').get_text()
     print(first_quote)
 
-get_nb_pages()
 
-get_nb_quotations()
-
-get_first_quote()
+count_pages()
+count_quotations()
+print_first_quote()
